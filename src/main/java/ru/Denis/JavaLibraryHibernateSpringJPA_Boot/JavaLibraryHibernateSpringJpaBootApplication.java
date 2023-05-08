9ebe1d52d8f3d31268a9a7cad987a1298c219b3a
DenWorker <whiteskyD@yandex.ru>
2023-05-08 13:52:20 +0300

    Create empty application

---
+++ src/main/java/ru/Denis/JavaLibraryHibernateSpringJPA_Boot/JavaLibraryHibernateSpringJpaBootApplication.java
@@ -0,0 +1,13 @@
+package ru.Denis.JavaLibraryHibernateSpringJPA_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLibraryHibernateSpringJpaBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLibraryHibernateSpringJpaBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/Denis/JavaLibraryHibernateSpringJPA_Boot/JavaLibraryHibernateSpringJpaBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Denis.JavaLibraryHibernateSpringJPA_Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLibraryHibernateSpringJpaBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
